451f610f8f842d5457cf750beba2081df8ded58a
bhayden2458 <blkhayden764@gmail.com>
2021-01-28 06:29:34 -0500

    Code now works for project

--- 3dkinematics.py
+++ 3dkinematics.py
@@ -1,5 +1,8 @@
 import numpy as np
 import math
+from arm_controller import ArmController
+
+
 
 # Translations
 def trans(l, d):
@@ -25,23 +28,28 @@ if __name__ == '__main__':
     num_j = 4 # Number of moveable joints on the robot
     result = np.eye(4) # Identity matrix
     origin = np.array([[0],[0],[0],[1]]) # Starting position
+    ac=ArmController()
 
     # Write code to perform the kinematics for each joint
-    ang = input("First joint angle (radians): ")
-    result = result.dot(rotZ(ang)).dot(trans(0, .077)).dot(rotX(-1*np.pi/2))
+    ang1 = input("First joint angle (radians): ")
+    result = result.dot(rotZ(ang1)).dot(trans(0, .077)).dot(rotX(-1*np.pi/2))
 
-    ang = input("Second joint angle (radians): ")
-    result = result.dot(rotZ(ang - 1.395)).dot(trans(.13, 0)).dot(rotX(np.pi))
+    ang2 = input("Second joint angle (radians): ")
+    result = result.dot(rotZ(ang2 - 1.385)).dot(trans(.13, 0)).dot(rotX(0))
     
-    ang = input("Third joint angle (radians): ")
-    result = result.dot(rotZ(ang + 1.395)).dot(trans(.124, 0)).dot(rotX(0))
+    ang3 = input("Third joint angle (radians): ")
+    result = result.dot(rotZ(ang3 + 1.385)).dot(trans(.124, 0)).dot(rotX(0))
     
-    ang = input("Fourth joint angle (radians): ")
-    result = result.dot(rotZ(ang)).dot(trans(.126, 0)).dot(rotX(np.pi/2))
+    ang4 = input("Fourth joint angle (radians): ")
+    result = result.dot(rotZ(ang4)).dot(trans(.126, 0)).dot(rotX(np.pi/2))
 
+    # Move the arm
+    ac.set_joints([ang1, ang2, ang3, ang4])
+    
     # T is transition matrix, final is the final position
     T = result
     final = result.dot(origin)
+    final[0][0] = final[0][0] + .012  # Correct for the difference in gazibo
 
     # Print Position
     print("Position (x,y,z): (" + str(final[0][0]) + ", " + str(final[1][0]) + ", " + str(final[2][0]) + ")")
@@ -51,5 +59,5 @@ if __name__ == '__main__':
     # cos(phi) which is used to find the other angles
     c = math.cos(phi)
     psi = np.arctan2(T[2][1]/c, T[2][2]/c)
-    theta = np.arctan2(T[0][0]/c, T[1][0]/c)
+    theta = np.arctan2(T[1][0]/c, T[0][0]/c)
     print("Orientation (roll, pitch, yaw): (" + str(psi) + "," + str(phi) + "," + str(theta) + ")")
--- arm_demo.py
+++ arm_demo.py
@@ -1,7 +1,6 @@
 from arm_controller import ArmController
 
 ac=ArmController()
-ac.set_joints([.2,-.5,.3,-.4])
-ac.set_joints([-.3,-.6,.7,.9])
 ac.set_joints([0,0,0,0])
+ac.set_joints([.5,.5,.25,0])
 print(ac.get_pose())
